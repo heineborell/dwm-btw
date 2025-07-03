4dc47a1ac3d68e78d73d25aea55e3833356d6a72
deniz deveci <dodeve@gmail.com>
2025-07-03 21:24:51 +0900

    arc final

--- config.h
+++ config.h
@@ -96,7 +96,7 @@ static const Key keys[] = {
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    TAGKEYS(XK_a, 0) TAGKEYS(XK_s, 1) TAGKEYS(XK_w, 2) TAGKEYS(XK_4, 3)
+    TAGKEYS(XK_a, 0) TAGKEYS(XK_s, 1) TAGKEYS(XK_w, 2) TAGKEYS(XK_e, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
 };
